eaf186117a9950bae967d917a205d198de2673f4
Jeff Wang <jeffwang4321@gmail.com>
2022-10-07 16:42:00 -0700

    Update text

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -8,21 +8,23 @@ const ProjectSection = () => {
       <h3>PROJECT EXPERIENCE</h3>
       <div id={styles.textbox}>
         <p><b>Multiplayer Browser Game</b> - IO Games - <a href="https://github.com/jeffwang4321/IO-Games" target="_"> <span>GitHub</span></a></p>
-        <p>May 2022 - Aug 2022</p>
+        <p>Nov 2020</p>
       </div>
       <ul>
-        <li>• &nbsp; Developed a web-based multiplayer browser game using Node.js, Express.js, and Socket.io</li>
-        <li>• &nbsp; Implemented a group chat feature, 3 unique minigames and automatic score tracking</li>
-        <li>• &nbsp; Organized client connections into unique rooms where individual minigames can run asynchronously</li>
-        <li>• &nbsp; Implemented real-time communication between client server environment using web sockets</li>
+        <li>• &nbsp; Developed a cross platform  multiplayer browser game using Node.js, Express.js, and Socket.io</li>
+        <li>• &nbsp; Implemented responsive design, a group chat feature, 3 unique minigames and automatic score tracking</li>
+        <li>• &nbsp; Organized client connections into unique game rooms where individual rooms can run asynchronously</li>
+        <li>• &nbsp; Implemented real-time communication between the client server environment using web sockets (TCP)</li>
       </ul>
       <ul id={styles.skills}>
         <li>Node.js</li>
         <li>Express.js</li>
         <li>Socket.io</li>
+        <li>TCP</li>
         <li>JavaScript</li>
-        <li>HTML5</li>
-        <li>CSS3</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Git</li>
         <li>Heroku</li>
       </ul>
     </section>
--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -8,12 +8,12 @@ const WorkSection = () => {
       <h3>WORK EXPERIENCE</h3>
       <div id={styles.textbox}>
         <p><b>Web Application Developer</b> - Government of Canada (PSPC - DSB)</p>
-        <p>May 2022 - Aug 2022</p>
+        <p>Jun 2022 - Sep 2022</p>
       </div>
       <ul>
-        <li>• &nbsp; Developed and optimized ASP.NET web applications for Canadian government clients in C#</li>
+        <li>• &nbsp; Developed, maintained, and optimized ASP.NET web applications for Canadian government clients in C#</li>
         <li>• &nbsp; Refactored and debugged MVC architecture, business billing logic, and SQL Server database connections</li>
-        <li>• &nbsp; Responsible for database scripting, network troubleshooting, jQuery, AJAX and Vue.js debugging</li>
+        <li>• &nbsp; Responsible for PHP database scripting, network troubleshooting, jQuery, AJAX, and Vue.js debugging</li>
       </ul>
       {/* <ul id={styles.skills}>
         <li>Vue.js</li>
@@ -33,21 +33,22 @@ const WorkSection = () => {
       <ul>
         <li>• &nbsp; Developed, refactored, and optimized React components, JavaScript functions, and AJAX promises. Resolving over 40+ JIRA tickets in a 4-month period</li>
         <li>• &nbsp; Implemented a pricing graph feature using Canvas API and optimized JSON history using Redux states</li>
-        <li>• &nbsp; Reduced the demand history graph's render time by 80% by implementing Redux caching</li>
+        <li>• &nbsp; Reduced the demand history graph's render time by 80% from 5s to 1s by implementing Redux caching</li>
         <li>• &nbsp; Developed the inactivity timeout function that automatically logs off the user after a set period of time</li>
-        <li>• &nbsp; Designed and automated frontend testing using Cypress and firmware tests using Python's Fabric library</li>
+        <li>• &nbsp; Designed and automated frontend testing using Cypress and firmware testing using Python's Fabric library</li>
         <li>• &nbsp; Automated REST API testing to monitor cloud health using Postman collections and environments</li>
-        <li>• &nbsp; Updated, troubleshooted and reprogrammed UNIX firmware using Bash scripts and XML commands</li>
+        <li>• &nbsp; Updated, troubleshooted, and reprogrammed UNIX firmware using Bash scripts and XML commands</li>
       </ul>
       <ul id={styles.skills}>
         <li>React.js</li>
         <li>Redux.js</li>
         <li>Cypress.io</li>
         <li>REST APIs</li>
-        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>AJAX</li>
         <li>Python</li>
-        <li>UNIX</li>
-        <li>Git</li>
+        {/* <li>UNIX</li> */}
+        <li>AWS</li>
       </ul>
 
       <div id={styles.textbox}>
@@ -55,9 +56,9 @@ const WorkSection = () => {
         <p>Apr 2019 - Dec 2019</p>
       </div>
       <ul>
-        <li>• &nbsp; Responsible for client software setup, updates, SQL Server maintenance and database migrations</li>
-        <li>• &nbsp; Setup, debugged and applied schema updates to SQL servers using SQL Server Management Studio</li>
-        <li>• &nbsp; Performed cloud database setup, backups, and automated database integrity testing using C++</li>
+        <li>• &nbsp; Responsible for client software setup, updates, Microsoft SQL Server database migrations</li>
+        <li>• &nbsp; Maintained, debugged and applied schema updates to SQL Servers using SQL Server Management Studio</li>
+        <li>• &nbsp; Performed cloud database setup, backups, and automated integrity testing on DB backups using C++</li>
       </ul>
       {/* <ul id={styles.skills}>
         <li>Microsoft SQL Server</li>
